d79125915081e375ceef4adaece2d4745b2cd209
L E <erickson.leah.j@gmail.com>
2023-03-18 17:24:30 -0600

    added quiz questions to javascript

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,59 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
     function showQuestions(questions, quizContainer){
 
-    // code goes here
+    var quizQuestions = [
+        {
+            question: "What is NOT a JavaScript Data Type?",
+            answers: {
+                a: "String"
+                b: "Number"
+                c: "Element"
+                d: "Boolean"
+            },
+            correctAnswer: 'c'
+        },
+        {
+            question: "Which symbol is used for comments in JavaScript?",
+            answers: {
+                a: "//"
+                b: "<!"
+                c: "#"
+                d: "&"
+
+            },
+            correctAnswer: 'a'
+        },
+        {
+            question: "Which of the following is a looping structure in JavaScript?",
+            answers: {
+                a: "Also"
+                b: "Too"
+                c: "Four"
+                d: "For"
+            },
+            correctAnswer: 'd'
+        },
+        {
+            question: "In JavaScript, what does NULL mean?",
+            answers: {
+                a: "Equal Value"
+                b: "No Value"
+                c: "Maximun Value"
+                d: "Undefined"
+            },
+            correctAnswer: 'b'
+        },
+        {
+            question: "How do you declare a varialbe in Javascript?",
+            answers: {
+                a: "var x = y"
+                b: "var & x = y"
+                c: "x var = y"
+                d: "y = var + x"
+            },
+            correctAnswer: 'a'
+        }
+    ];
 }
 
 function showResults(questions, quizContainer, resultsContainer){
