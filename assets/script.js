4c98c5a565f1242cdf5ca79e2111312109c4a739
L E <erickson.leah.j@gmail.com>
2023-03-18 17:03:03 -0600

    added line to click submit to show results

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,8 @@ function showResults(questions, quizContainer, resultsContainer){
 
 //show questions
 showQuestions(questions, quizContainer);
+
+//when user clicks submit, show results
+submitButton.onclick = function(){
+    showResults(questions,quizContainer, resultsContainer);
+}
