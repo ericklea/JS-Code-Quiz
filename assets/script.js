d8b79d07b5f7f57efdf3520b2c922bc29bc30553
L E <erickson.leah.j@gmail.com>
2023-03-20 04:06:11 -0600

    reorganized code

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,37 @@
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+  };
+
+  loadQuiz();
+  
+const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+      if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+  };
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+  };
+  const quiz = document.getElementById("quiz");
+  const answerElements = document.querySelectorAll(".answer");
+  const questionElement = document.getElementById("question");
+  const a_text = document.getElementById("a_text");
+  const b_text = document.getElementById("b_text");
+  const c_text = document.getElementById("c_text");
+  const d_text = document.getElementById("d_text");
+  const submitButton = document.getElementById("submit");
+  
+  let currentQuiz = 0;
+  let score = 0;
+
 const quizData = [
     {
         question: "What is NOT a JavaScript Data Type?",
@@ -41,39 +75,6 @@ const quizData = [
         correct: "a",
     },
 ];
-const quiz = document.getElementById("quiz");
-const answerElements = document.querySelectorAll(".answer");
-const questionElement = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitButton = document.getElementById("submit");
-
-let currentQuiz = 0;
-let score = 0;
-
-const deselectAnswers = () => {
-    answerElements.forEach((answer) => (answer.checked = false));
-  };
-  const getSelected = () => {
-    let answer;
-    answerElements.forEach((answerElement) => {
-      if (answerElement.checked) answer = answerElement.id;
-    });
-    return answer;
-  };
-  const loadQuiz = () => {
-    deselectAnswers();
-    const currentQuizData = quizData[currentQuiz];
-    questionElement.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-  };
-  
-  loadQuiz();
 
   submitButton.addEventListener("click", () => {
     const answer = getSelected();
